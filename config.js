fa7417371edc1eb0465fdae6de1abbd4d9b5043e
Dele <ddelemissis@gmail.auth>
2020-04-10 14:09:54 +0300

    Disable fb

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  siteTitle: 'Dimitris Delemissis', // <title>
-  manifestName: 'Personal Website',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/images/fav_icon.png',
+  siteTitle: "Dimitris Delemissis", // <title>
+  manifestName: "Personal Website",
+  manifestShortName: "Landing", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#663399",
+  manifestThemeColor: "#663399",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/images/fav_icon.png",
   pathPrefix: `dele/`, // This path is subpath of your hosting https://domain/portfolio
-  firstName: 'Dimitris',
-  lastName: 'Delemissis',
+  firstName: "Dimitris",
+  lastName: "Delemissis",
   // social
   socialLinks: [
     {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/delemissis',
+      icon: "fa-github",
+      name: "Github",
+      url: "https://github.com/delemissis",
     },
     {
-      icon: 'fa-linkedin-in',
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/dimitris-delemissis-baa83919a/',
+      icon: "fa-linkedin-in",
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/dimitris-delemissis-baa83919a/",
     },
-    {
-      icon: 'fa-facebook-f',
-      name: 'Facebook',
-      url: 'https://www.facebook.com/dimitris.delemissis',
-    },
+    // {
+    //   icon: 'fa-facebook-f',
+    //   name: 'Facebook',
+    //   url: 'https://www.facebook.com/dimitris.delemissis',
+    // },
   ],
-  email: 'ddelemissis@gmail.com',
+  email: "ddelemissis@gmail.com",
 };
